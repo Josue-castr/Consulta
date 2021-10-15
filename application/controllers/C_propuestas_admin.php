ce54d8ca9ea92fc410b9585b3e3fde07fad3350f
Josue Castro <josueescalante-150@hotmail.com>
2021-10-15 18:24:21 -0500

    Uptdate en la parte de proupuestas
    Sirve para que cargue los datos

--- application/controllers/C_propuestas_admin.php
+++ application/controllers/C_propuestas_admin.php
@@ -41,8 +41,11 @@ class C_propuestas_admin extends CI_Controller
 		//$where="";
 		$tipo = '';
 		$palabra = trim($this->input->post('fTitulo'));
-		if ($this->input->post('fSector') > 0) $where = 's."iIdSector"=' . $this->input->post('fSector');
-		if ($this->input->post('fTema') > 0) $where = 'p."iIdTema"=' . $this->input->post('fTema');
+		if ($this->input->post('fSector') > 0) $where['iIdSector']= $this->input->post('fSector');
+		if ($this->input->post('fTema') > 0) $where['p.iIdTema']= $this->input->post('fTema');
+
+		//if ($this->input->post('fSector') > 0) $where = 's."iIdSector"=' . $this->input->post('fSector');
+		//if ($this->input->post('fTema') > 0) $where = 'p."iIdTema"=' . $this->input->post('fTema');
 		if($this->input->post('fEstatus') !=  10){ $where['p.iEstatus']= $this->input->post('fEstatus');}
 		//echo "<script>alert('".$where['p.Estatus']."');</script>";
 		//opcion 1 //if($this->input->post('fEstatus') !=  10){ $where = '"p.iEstatus"=' .$this->input->post('fEstatus'); $tipo='"p.iEstatus"'; }
